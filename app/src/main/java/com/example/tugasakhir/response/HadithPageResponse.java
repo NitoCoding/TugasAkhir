04e99769c2d5f7d1cd01131557c14d5978265cfc
NitoCoding <ilhamhendra03@gmail.com>
2024-06-02 23:56:44 +0800

    app

---
+++ app/src/main/java/com/example/tugasakhir/response/HadithPageResponse.java
@@ -0,0 +1,19 @@
+package com.example.tugasakhir.response;
+
+import com.example.tugasakhir.models.Book;
+import com.example.tugasakhir.models.Chapter;
+
+import java.util.List;
+
+public class HadithPageResponse {
+    private HadithResponse hadiths;
+
+    public HadithResponse getHadiths() {
+        return hadiths;
+    }
+
+    public void setHadiths(HadithResponse hadiths) {
+        this.hadiths = hadiths;
+    }
+}
+
